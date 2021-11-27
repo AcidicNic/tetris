f62888ddbf01d28ed8830c60de97e7b6c75ed410
Nicole Abadie <nic4096@gmail.com>
2021-11-26 16:18:05 -0800

    Added NextBlock

--- src/components/GridBoard.js
+++ src/components/GridBoard.js
@@ -8,14 +8,14 @@ export default function GridBoard(props) {
   for (let row = 0; row < 18; row ++) {
     grid.push([])
     for (let col = 0; col < 10; col ++) {
-      grid[row].push(<GridSquare key={`${col}${row}`} color="1" />)
+      grid[row].push(<GridSquare key={`${ col }${ row }`} color="1" />)
     }
   }
 
   // The components generated in makeGrid are rendered in div.grid-board
   return (
     <div className='grid-board'>
-      {grid}
+      { grid }
     </div>
   )
 }
